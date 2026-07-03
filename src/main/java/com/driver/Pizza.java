d2e48414a9ef940e34ebfd8032576b6886d8cc9a
PRATHAM <prathameshdeore268@gmail.com>
2026-07-03 22:57:30 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -3,86 +3,83 @@ package com.driver;
 public class Pizza {
 
     private int price;
+    private int basePrice;
     private Boolean isVeg;
     private String bill;
 
-    // Add these variables
-    private int basePrice;
     private boolean extraCheeseAdded = false;
     private boolean extraToppingsAdded = false;
     private boolean takeawayAdded = false;
+    private boolean billGenerated = false;
 
-    public Pizza(Boolean isVeg){
+    public Pizza(Boolean isVeg) {
         this.isVeg = isVeg;
-        bill = "";
+        this.bill = "";
 
-        if(isVeg){
-            price = 300;
-        }
-        else{
-            price = 400;
+        if (isVeg) {
+            this.price = 300;
+        } else {
+            this.price = 400;
         }
 
-        basePrice = price;
+        this.basePrice = this.price;
     }
 
-    public int getPrice(){
+    public int getPrice() {
         return this.price;
     }
 
-    public void addExtraCheese(){
-
-        if(!extraCheeseAdded){
+    public void addExtraCheese() {
+        if (!extraCheeseAdded) {
             price += 80;
             extraCheeseAdded = true;
         }
     }
 
-    public void addExtraToppings(){
-
-        if(!extraToppingsAdded){
-
-            if(isVeg){
+    public void addExtraToppings() {
+        if (!extraToppingsAdded) {
+            if (isVeg) {
                 price += 70;
-            }
-            else{
+            } else {
                 price += 120;
             }
-
             extraToppingsAdded = true;
         }
     }
 
-    public void addTakeaway(){
-
-        if(!takeawayAdded){
+    public void addTakeaway() {
+        if (!takeawayAdded) {
             price += 20;
             takeawayAdded = true;
         }
     }
 
-    public String getBill(){
+    public String getBill() {
 
-        bill += "Base Price Of The Pizza: " + basePrice + "\n";
+        if (!billGenerated) {
 
-        if(extraCheeseAdded){
-            bill += "Extra Cheese Added: 80\n";
-        }
+            bill += "Base Price Of The Pizza: " + basePrice + "\n";
 
-        if(extraToppingsAdded){
-            if(isVeg){
-                bill += "Extra Toppings Added: 70\n";
+            if (extraCheeseAdded) {
+                bill += "Extra Cheese Added: 80\n";
             }
-            else{
-                bill += "Extra Toppings Added: 120\n";
+
+            if (extraToppingsAdded) {
+                if (isVeg) {
+                    bill += "Extra Toppings Added: 70\n";
+                } else {
+                    bill += "Extra Toppings Added: 120\n";
+                }
             }
-        }
 
-        if(takeawayAdded){
-            bill += "Paperbag Added: 20\n";
-        }
+            if (takeawayAdded) {
+                bill += "Paperbag Added: 20\n";
+            }
 
-        bill += "Total Price: " + price;
+            bill += "Total Price: " + price + "\n";
+
+            billGenerated = true;
+        }
 
         return bill;
     }
